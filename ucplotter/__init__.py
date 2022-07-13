c9f5cfa233c55e142a5c16e49635f653b2187d7d
Rajesh Nakka <338rajesh@gmail.com>
2022-07-13 22:42:12 +0530

    made some preliminary changes

---
+++ ucplotter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Unit Cell Plotter
+=================
+
+Plotting and manipulating images of composite microstructure.
+"""
+
+from .ucplotter import TwoPhase_RVEImage
+
+__all__ = ["TwoPhase_RVEImage",]
