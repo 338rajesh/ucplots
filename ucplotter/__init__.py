7fff890fb1005b50e6ee71848c3ffb5503330c4c
Rajesh Nakka <338rajesh@gmail.com>
2022-07-18 19:59:38 +0530

    modified __init__.py

--- ucplotter/__init__.py
+++ ucplotter/__init__.py
@@ -1,15 +1,15 @@
-"""
-Unit Cell Plotter
-=================
+# """
+# Unit Cell Plotter
+# =================
 
-Plotting and manipulating images of composite microstructure.
-"""
+# Plotting and manipulating images of composite microstructure.
+# """
 
-from .ucimage import TwoPhase_UCImage
-from .ucplotter import plot_unit_cell, plot_unit_cells_from_h5
+# from .ucimage import TwoPhase_UCImage
+# from .ucplotter import plot_unit_cell, plot_unit_cells_from_h5
 
-__all__ = [
-    "TwoPhase_UCImage",
-    "plot_unit_cell",
-    "plot_unit_cells_from_h5",
-]
+# __all__ = [
+#     "TwoPhase_UCImage",
+#     "plot_unit_cell",
+#     "plot_unit_cells_from_h5",
+# ]
