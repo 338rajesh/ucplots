85e7a6903b47a483e9f37b4690d5af5cda9e869c
Rajesh Nakka <338rajesh@gmail.com>
2023-08-25 11:13:59 +0530

    Updated test file

--- tests/test_ucplotter.py
+++ tests/test_ucplotter.py
@@ -3,12 +3,10 @@ from ucplots.ucplots import UCPlot
 from os import path
 
 print("Testing CShape")
-ucp = UCPlot()
-ucp.plot_unit_cell(
-    uc_bbox=(-10.0, -10.0, 10.0, 10.0),
-    inclusions_data={"C": [
-        [1.0, 1.0, 0.0 * pi, 6.0, 3.0, 0.5 * pi, ]], },
-    image_path=path.join(path.expanduser("~"), "test_c_shape.png"),
-    fibre_color="0.1",
-    matrix_color="0.6",
-)
+
+if __name__ == '__main__':
+    UCPlot().plot_unit_cell(
+        uc_bbox=(-1.0, -1.0, 1.0, 1.0),
+        inclusions_data={"CIRCLE": [[0.0, 0.0, 1.0]], },
+        image_path=path.join(path.dirname(__file__), "test_uc_image.png")
+    )
